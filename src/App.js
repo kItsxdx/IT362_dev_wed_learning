64c45a137f45a6f9decb0e9ec62f3eba0a19e7bd
kItsxdx <155843650+kItsxdx@users.noreply.github.com>
2025-01-15 19:25:15 +0700

    แก้ไข คำผิด

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          <h1>แอปบัญชีรสยรับรสยจ่าย</h1>
+          <h1>แอปบัญชีรายรับรายจ่าย</h1>
           <h2>วันที่ 10 มหราคม</h2>
           <Transation/>
           </div>
