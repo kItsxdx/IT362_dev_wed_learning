dfeaa4ed8dcbcad9848183f4340be919713980d2
kItsxdx <66111375@dpu.ac.th>
2025-01-19 10:07:44 +0700

    #Update Week_2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Transation from './component/Transation';
+import FormComponent from './component/InputForm'
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
           <h1>แอปบัญชีรายรับรายจ่าย</h1>
           <h2>วันที่ 10 มหราคม</h2>
+          <FormComponent/>
           <Transation/>
           </div>
       
--- src/component/InputForm.js
+++ src/component/InputForm.js
@@ -17,3 +17,4 @@ const FormComponent = ()=>{
 
     )
 }
+export default FormComponent
