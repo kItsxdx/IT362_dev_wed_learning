99e4247bd9b8ac8617ccdfb61658661bcb431bd0
kItsxdx <155843650+kItsxdx@users.noreply.github.com>
2025-01-20 15:02:04 +0700

    #เพิ่ม setTitle , setAmount #จัดการย่อหน้าใหม่

--- src/component/FromComponent.js
+++ src/component/FromComponent.js
@@ -1,10 +1,10 @@
-import { useState } from "react"
-import { v4 as uuidv4 } from "uuid"
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const FromComponane =(props)=>{
 
     const [title,setTitle]=useState('')
-    const [amount,setAmount ]=useState(0)
+    const [amount,setAmount ]=useState()
     
     const inputTitle =(event)=>{
         setTitle(event.target.value)
@@ -13,7 +13,7 @@ const FromComponane =(props)=>{
         setAmount(event.target.value)
     }
     const saveItem=(event)=>{
-        event.preventDefault()
+        event.preventDefault();
         const itemData = {
         id:uuidv4(),
         title:title,
@@ -21,7 +21,9 @@ const FromComponane =(props)=>{
         }
         console.log(itemData)
     
-        props.onAddItem(itemData); 
+        props.onAddItem(itemData);
+        setTitle("");
+        setAmount("");
         
         }
     return(
@@ -29,14 +31,29 @@ const FromComponane =(props)=>{
              <form onSubmit={saveItem} class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div className="w-full max-w-xs">
                 
-                <div className="mb-2">
-                    <label>ชื่อรายการ</label>
-                    <div><input type="text" onChange={inputTitle} value={title} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"placeholder=" โปรดระบุชื่อรายการ"/></div>
-                </div>
-                <div>
-                    <label>จำนวนเงิน</label>
-                    <div><input type="text" onChange={inputAmount} value={amount} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"placeholder =" +รายรับ,-รายจ่าย"/></div>
-                </div>
+                        <div>
+                            <label>ชื่อรายการ</label>
+                        
+                                <input 
+                                    type="text" 
+                                    onChange={inputTitle} 
+                                    value={title} 
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    placeholder=" โปรดระบุชื่อรายการ"/>
+                        </div>
+                    
+                        <div>
+                            <label>จำนวนเงิน</label>
+                        
+                                <input 
+                                    type="text" 
+                                    onChange={inputAmount} 
+                                    value={amount}
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    placeholder ="+รายรับ,-รายจ่าย"/>
+                            
+                        
+                        </div>
                 <div>
                     <button type="submit"className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-3 my-1">เพื่มข้อมูล</button>
                 </div>
