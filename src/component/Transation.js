0f7d8dc18cab2646a3beaec4a2b60ee6227837a2
kItsxdx <155843650+kItsxdx@users.noreply.github.com>
2025-01-26 11:01:30 +0700

    #add div

--- src/component/Transation.js
+++ src/component/Transation.js
@@ -1,14 +1,15 @@
 import Item from './Item';
 import { v4 as uuidv4 } from 'uuid';
 
-
 const Transaction = (props) => {
     const { items } = props;
     return (
-        <ul style={{ listStyle: "none", padding: 0 }}>
-            {items.map((e) => (<Item {...e} key={uuidv4()}
-            />))}
-        </ul>
+        <div>
+            <ul style={{ listStyle: "none", padding: 0 }}>
+                {items.map((e) => (<Item {...e} key={uuidv4()}
+                />))}
+            </ul>
+        </div>
     );
 }
 
