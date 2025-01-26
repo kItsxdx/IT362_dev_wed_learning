36dcef9c4bbf1e9dc32a0ff9734b2cb857c0a1a9
kItsxdx <155843650+kItsxdx@users.noreply.github.com>
2025-01-26 14:52:49 +0700

    #import css.

--- src/component/ReportComponent.js
+++ src/component/ReportComponent.js
@@ -1,12 +1,21 @@
 import DataContext from "../data/DataContext";
 import { useContext } from "react";
+import './ReportComponent.css'
 const ReportComponent=()=>{
     const {income,expense} = useContext(DataContext)
-
+    const output = (income - expense).toLocaleString()
     return(
         <div>
-            <p>รายรับ:{income}</p>
-            <p>รายจ่าย:{expense}</p>
+            <div>
+                <p>ยอดเงินคงเหลือ (บาท) : ฿{output}</p>
+
+            </div>
+            <div>
+                    <p className="box green">
+                        รายรับ:{income}</p>
+                    <p className="box red">
+                        รายจ่าย:{expense}</p>
+            </div>
         </div>
     );
 }
