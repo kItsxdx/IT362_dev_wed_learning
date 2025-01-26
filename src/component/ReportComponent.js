4e855f5843cda7179bc2376896d421e39e201254
kItsxdx <155843650+kItsxdx@users.noreply.github.com>
2025-01-26 10:58:03 +0700

    #create file ReportComponent

---
+++ src/component/ReportComponent.js
@@ -0,0 +1,13 @@
+import DataContext from "../data/DataContext";
+import { useContext } from "react";
+const ReportComponent=()=>{
+    const {income,expense} = useContext(DataContext)
+
+    return(
+        <div>
+            <p>รายรับ:{income}</p>
+            <p>รายจ่าย:{expense}</p>
+        </div>
+    );
+}
+export default ReportComponent
