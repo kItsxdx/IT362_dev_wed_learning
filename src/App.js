c186b1055a5433a67305cf02735d3ea75ea3ab89
kItsxdx <155843650+kItsxdx@users.noreply.github.com>
2025-02-02 17:23:25 +0700

    #update new file App.js , AddUser.js , NavbarComponent.js , Usr.js , #install new module

--- src/App.js
+++ src/App.js
@@ -1,89 +1,17 @@
-import Transaction from './component/Transation'
-import FromComponane from './component/FromComponent';
-import './App.css';
-import { useEffect, useState,useReducer} from "react";
-import DataContext from './data/DataContext';
-import ReportComponent from './component/ReportComponent';
-import  { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
-
-function App() {
-  const initData = [
-    { id: 1, title: "ค่าเดินทาง", amount: 100 },
-    { id: 2, title: "ค่าอาหาร", amount: 200 },
-    { id: 3, title: "ค่าชอปปิ้ง", amount: -300 },
-  ];
-
-  const [items, setItems] = useState(initData);
-  const [reportIncome,setReportIncome] = useState(0)
-  const [reportExpense,setReportExpense] = useState(0)
-
-  const onAddNewItem = (newItem) => {
-    setItems((prevItem) => {
-      return [newItem, ...prevItem];
-    });
-  };
-  useEffect(()=>{
-    const amounts = items.map(items=>items.amount)
-    const income = amounts.filter(element=>element>0).reduce((total,element)=>total+=element,0)
-    const expense = (amounts.filter(element=>element<0).reduce((total,element)=>total+=element,0))*-1
-    setReportIncome(income)
-    setReportExpense(expense)    
-  },[items,reportIncome,reportExpense])
-
-//Use reducer stat
-// const [showReport,setShowReport]= useState(false)
-// const reducer = (state,action)=>{
-//       switch(action.type){case "SHOW" :
-//         return setShowReport(!showReport)}
-//       }
-// const
-// [result,dispatch]=useReducer(reducer,showReport)
- 
-  return (
-    <div>
-      {/* <div align="center">
-<h1>{result}</h1>
-      <button className='btn'
-      onClick={()=>dispatch({type:"SHOW"})}>แสดง/ซ่อน</button>
-      
-    </div> */}
-    
-      <DataContext.Provider value={
-        {
-          income:reportIncome,
-          expense:reportExpense
-        }
-      }>
-        <div className="App">
-          <h1 className="text-4xl font-bold underline bg-white px-8 pt-6 pb-8 mb-4 text-center ">
-            แอพบัญชีรายรับ-รายจ่าย
-          </h1>
-          <h2 className='text-xl text-center underline'>
-            วันที่ 10 มกราคม 2568</h2>
-            <Router>
-              <div>
-                <ul>
-                  <li className='btnA'><Link to ="/">ข้อมูลบัญชี</Link></li>
-                  <li className='btnB'><Link to ="/insert">บันทึกข้อมูล</Link></li>
-                </ul>
-                <Routes>
-                  <Route path='/' element={<ReportComponent/>}/>
-                  <Route path='/insert' element={
-                    <div>
-                      <FromComponane onAddItem={onAddNewItem}/>
-                      <Transaction items = {items}/>
-                    </div>
-                  }/>
-                </Routes>
-              </div>
-
-            </Router>
-          <FromComponane onAddItem={onAddNewItem} />
-          <Transaction items={items} />
+import NavbarComponent from './component/NavbarComponent';
+import {Routes,Route} from 'react-router-dom'
+import User from './component/User';
+import AddUser from './adduser/AddUser';
+function App(){
+    return(
+        <div>
+            <NavbarComponent/>
+            <Routes>
+                <Route path='/user' element={<User/>}></Route>
+                <Route path='/add' element={<AddUser/>}></Route>
+            </Routes>
         </div>
-      </DataContext.Provider>
-      </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/adduser/AddUser.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import '../App.css';
+
+const AddUser=()=>{
+    
+    
+        const users={
+        name:"",
+        email:"",
+        address:"",
+        };
+        const [user,setUser]=useState(users)
+        const navigate = useNavigate()
+        const inputUser = (e)=>{
+        const{name,value}=e.target
+        console.log(name,value)
+        setUser({...user,[name]:value})
+        }
+
+        const submitForm=async(e)=>{
+            e.preventDefault();
+            await axios.post("http://localhost:8000/api/user",user)
+            .then((response)=>{
+            toast.success(response.data.message,{position:"top-right"})
+            console.log("User created successfully.")
+            navigate("/user")
+            
+            })
+            .catch((error)=>{
+            console.log(error)
+            })
+            }
+            return(<div className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+    <h3 className='text-4xl font-bold underline bg-white px-8 pt-6 pb-8 mb-4 text-center'>Add New User</h3>
+    <form onSubmit={submitForm}>
+    <div >
+            <label >Name</label>
+        <input type="text" id="name" name="name" required onChange={inputUser}/>
+            
+            <label >Your email</label>
+        <input type="email" id="email" name="email" required onChange={inputUser}/>
+            
+            <label for="address">Your address</label>
+        <input type="text" id="address" name="address" required onChange={inputUser} />
+    </div>
+    <button 
+    className="bg-white hover:bg-green-100 text-green-800 font-semibold py-2 px-4 border border-green-400 rounded shadow mt-3 my-1"
+    
+    type="submit">Submit</button>
+    </form>
+    </div>
+    );}
+
+
+export default AddUser
